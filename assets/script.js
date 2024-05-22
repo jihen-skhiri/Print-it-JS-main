d9834cab2d8d563d5547962ec9627aaaed31f64f
jihen Skhiri <jihen.skhiri@gmail.com>
2024-05-22 15:48:34 +0200

    changeSlide commit

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,35 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+/*fonction Ajout des bullets points */
+
+AddBullets();
+function AddBullets() {
+	let dots = document.querySelector(".dots");
+	for(let i=0; i<slides.length;i++) {
+		let newdot = document.createElement("span")
+		newdot.classList.add("dot")
+		dots.appendChild(newdot)
+	}	
+	console.log(dots)
+}
+
+/***************fonction pour modifier le slide***********************/
+let right = document.getElementById("slideright")
+let left = document.getElementById("slideleft")
+let numero = 0;
+let sens = 0
+function changeSlide(sens) {
+	numero = numero + sens;
+	let tag = document.querySelector("#banner p")
+	for(let i=0; i<slides.length;i++) {
+		document.getElementById("slide").src = "./assets//images/slideshow/" + slides[numero].image;
+		tag.innerHTML = slides[numero].tagLine;
+	}
+}
+right.addEventListener("click", changeSlide(sens));
+left.addEventListener("click", changeSlide(sens));
+
+
