9472ab432d35d76d64b34d29f55907c2c738bf83
jihen Skhiri <jihen.skhiri@gmail.com>
2024-05-24 10:32:09 +0200

    bullets commit

--- assets/script.js
+++ assets/script.js
@@ -19,17 +19,42 @@ const slides = [
 
 
 /*fonction Ajout des bullets points */
-
+let dots = document.querySelector(".dots");
 AddBullets();
 function AddBullets() {
-	let dots = document.querySelector(".dots");
+	
 	for(let i=0; i<slides.length;i++) {
 		let newdot = document.createElement("span")
 		newdot.classList.add("dot")
+		if (i === 0) newdot.classList.add('dot_selected');
+        newdot.dataset.i = i;
 		dots.appendChild(newdot)
 	}	
-	console.log(dots)
 }
+/************************Update Bullets*************************/
+let point = document.querySelectorAll(".dot")
+function currentslide(n){
+	
+	for(i=0; i<point.length;i++) {
+		point[i].classList.toggle('dot_selected', i===n);	
+		
+	};
+	
+	}
+/*******Change slide en click sur Bullet */
+
+function ClickBullet(){
+for (let i = 0; i < point.length; i++) {
+        point[i].addEventListener('click', (e) => {
+            numero = parseInt(e.target.dataset.i);
+           changeSlide(numero);
+        });
+
+    }
+}
+
+ClickBullet()
+
 
 /***************fonction pour modifier le slide***********************/
 let right = document.getElementById("slideright")
@@ -39,12 +64,22 @@ let sens = 0
 function changeSlide(sens) {
 	numero = numero + sens;
 	let tag = document.querySelector("#banner p")
-	for(let i=0; i<slides.length;i++) {
+	for(let i=0; i<slides.length; i++) {
 		document.getElementById("slide").src = "./assets//images/slideshow/" + slides[numero].image;
-		tag.innerHTML = slides[numero].tagLine;
-	}
+		tag.innerHTML = slides[numero].tagLine;	
+		currentslide(numero)
+		
+	}	
+	
+	
 }
+/*******défilement infini */
+
+
 right.addEventListener("click", changeSlide(sens));
 left.addEventListener("click", changeSlide(sens));
+
+
+
 
 
